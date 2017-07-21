fc663e3274717e9af202aaa72f46833d3fbb7c99
SoumyaAgarwal <soumya.agarwal@gmail.com>
2017-07-21 18:34:04 +0530

    merge

---
+++ app/src/main/java/com/example/pulkit/employeeapp/Notification/NotificationActivity.java
@@ -0,0 +1,87 @@
+package com.example.pulkit.employeeapp.Notification;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.pulkit.employeeapp.R;
+import com.example.pulkit.employeeapp.adapters.notification_adapter;
+import com.example.pulkit.employeeapp.model.Notif;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.pulkit.employeeapp.EmployeeApp.DBREF;
+
+public class NotificationActivity extends AppCompatActivity implements notification_adapter.NotificationAdapterListener{
+
+    RecyclerView recview;
+    notification_adapter adapter;
+    List<Notif> list = new ArrayList<>();
+    Notif notif  = new Notif();
+    String Username;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification2);
+
+        Username = getIntent().getStringExtra("Username");
+
+        recview = (RecyclerView) findViewById(R.id.notification_list);
+        recview.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recview.setItemAnimator(new DefaultItemAnimator());
+        recview.addItemDecoration(new DividerItemDecoration(getApplicationContext(), LinearLayoutManager.VERTICAL));
+
+        adapter = new notification_adapter(list, getApplicationContext(),this);
+        recview.setAdapter(adapter);
+
+        preparelist();
+    }
+
+    private void preparelist()
+    {
+        final DatabaseReference db = DBREF.child("Notification").child(Username).getRef();
+        db.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s)
+            {
+                notif = dataSnapshot.getValue(Notif.class);
+                list.add(notif);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onNotificationRowClicked(int position) {
+
+    }
+}
---
+++ app/src/main/java/com/example/pulkit/employeeapp/adapters/notification_adapter.java
@@ -0,0 +1,87 @@
+package com.example.pulkit.employeeapp.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.pulkit.employeeapp.R;
+import com.example.pulkit.employeeapp.model.Notif;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by SoumyaAgarwal on 7/20/2017.
+ */
+
+public class notification_adapter extends  RecyclerView.Adapter<notification_adapter.MyViewHolder> {
+    List<Notif> list = new ArrayList<>();
+    private Context context;
+    private NotificationAdapterListener listener;
+
+    public notification_adapter(List<Notif> list, Context c, NotificationAdapterListener listener) {
+        this.list = list;
+        this.context = c;
+        this.listener = listener;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView notif_message, notif_timestamp, icon_text, notif_sender;
+        ImageView imgProfile;
+        LinearLayout row;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            notif_message = (TextView)itemView.findViewById(R.id.notification_message);
+            notif_timestamp = (TextView) itemView.findViewById(R.id.notification_time);
+            icon_text = (TextView) itemView.findViewById(R.id.icon_text);
+            imgProfile = (ImageView) itemView.findViewById(R.id.icon_profile);
+            notif_sender = (TextView) itemView.findViewById(R.id.notification_sender);
+            row = (LinearLayout)itemView.findViewById(R.id.row);
+        }
+
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notification_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final notification_adapter.MyViewHolder holder, final int position)
+    {
+        Notif notif = list.get(position);
+
+        holder.notif_sender.setText(notif.getSenderId());
+        holder.notif_message.setText(notif.getContent());
+        holder.notif_timestamp.setText(notif.getTimestamp());
+        Character caps = notif.getSenderId().toUpperCase().charAt(0);
+        holder.icon_text.setText(caps);
+        applyClickEvents(holder,position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public interface NotificationAdapterListener {
+        void onNotificationRowClicked(int position);
+    }
+    private void applyClickEvents(MyViewHolder holder, final int position) {
+
+        holder.row.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                listener.onNotificationRowClicked(position);
+            }
+        });
+    }
+}
